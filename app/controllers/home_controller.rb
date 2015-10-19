6253b266c40ba0a18ecd7f393c2cbe99dd061d41
= <=>
2015-10-19 14:24:49 +0000

    Removed replicated and unused code.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,54 +5,20 @@ require 'base64'
 
 class HomeController < ApplicationController
 
-# Your AWS Access Key ID, as taken from the AWS Your Account page
-AWS_ACCESS_KEY_ID = "AKIAIIZ7OB22ZB3TA2FA"
 
-# Your AWS Secret Key corresponding to the above ID, as taken from the AWS Your Account page
-AWS_SECRET_KEY = "q7ntL+bVRuTtJXjz4sAqLvVtVlZzoLtQM3/C8CBr"
 
-# The region you are interested in
-ENDPOINT = "webservices.amazon.com"
+  # The region you are interested in
+  ENDPOINT = "webservices.amazon.com"
 
-REQUEST_URI = "/onca/xml"
-
-params = {
-  "Service" => "AWSECommerceService",
-  "Operation" => "ItemSearch",
-  "AWSAccessKeyId" => "AKIAIIZ7OB22ZB3TA2FA",
-  "AssociateTag" => "trailbackp-20",
-  "SearchIndex" => "SportingGoods",
-  "Keywords" => "backpack",
-  "ResponseGroup" => "Images,ItemAttributes,Offers",
-  "Sort" => "price"
-}
-
-# Set current timestamp if not set
-params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
-
-# Generate the canonical query
-canonical_query_string = params.sort.collect do |key, value|
-  [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-end.join('&')
-
-# Generate the string to be signed
-string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
-
-# Generate the signature required by the Product Advertising API
-signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), AWS_SECRET_KEY, string_to_sign)).strip()
-
-# Generate the signed URL
-request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
-
-puts "Signed URL: \"#{request_url}\""
-
+  REQUEST_URI = "/onca/xml"
 
 	layout "application"
+  
   def index
   end
 
   def search
-    @inputData = "blah blah blah"
+    @inputData = "Test"
 
     Aws.config.update({
       region: 'us-west-2',
@@ -60,51 +26,49 @@ puts "Signed URL: \"#{request_url}\""
     })
 
     params = {
-  "Service" => "AWSECommerceService",
-  "Operation" => "ItemSearch",
-  "AWSAccessKeyId" => "AKIAIIZ7OB22ZB3TA2FA",
-  "AssociateTag" => "trailbackp-20",
-  "SearchIndex" => "SportingGoods",
-  "Keywords" => "backpack",
-  "ResponseGroup" => "Images,ItemAttributes,Offers",
-  "Sort" => "price"
-}
+      "Service" => "AWSECommerceService",
+      "Operation" => "ItemSearch",
+      "SearchIndex" => "SportingGoods",
+      "Keywords" => "backpack",
+      "ResponseGroup" => "Images,ItemAttributes,Offers",
+      "Sort" => "price"
+    }
 
-# Set current timestamp if not set
-params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
+  # Set current timestamp if not set
+  params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
 
-# Generate the canonical query
-canonical_query_string = params.sort.collect do |key, value|
-  [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-end.join('&')
+  # Generate the canonical query
+  canonical_query_string = params.sort.collect do |key, value|
+    [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
+  end.join('&')
 
-# Generate the string to be signed
-string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
+  # Generate the string to be signed
+  string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
 
-# Generate the signature required by the Product Advertising API
-signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), AWS_SECRET_KEY, string_to_sign)).strip()
+  # Generate the signature required by the Product Advertising API
+  signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), "", string_to_sign)).strip()
 
-# Generate the signed URL
-request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
+  # Generate the signed URL
+  request_url = "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
 
-    puts "Signed URL: \"#{request_url}\""
+  puts "Signed URL: \"#{request_url}\""
 
-@inputData = request_url
+  @inputData = request_url
 
-request = Vacuum.new
-request.configure(
-  aws_access_key_id: AWS_ACCESS_KEY_ID,
-  aws_secret_access_key: AWS_SECRET_KEY,
-  associate_tag: 'trailbackp-20'
+  request = Vacuum.new
+  request.configure(
+    aws_access_key_id: "",
+    aws_secret_access_key: "",
+    associate_tag: ""
+    )
+  response = request.item_search(
+    query: {
+      'Keywords' => 'Architecture',
+      'SearchIndex' => 'Books'
+    }
   )
 
-response = request.item_search(
-  query: {
-    'Keywords' => 'Architecture',
-    'SearchIndex' => 'Books'
-  }
-)
-@inputData = response.to_h
+  @inputData = response.body
   end
 
 end
